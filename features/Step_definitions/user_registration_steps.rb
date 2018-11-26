4d22063a869791d93d0001f04044f1f5fbc197d8
Aditya Naik <naik.aditya@gmail.com>
2018-11-26 19:20:19 +0100

    Updates form format, adds name to create account button, adds test to step definitions to click on button, test is going green

--- features/Step_definitions/user_registration_steps.rb
+++ features/Step_definitions/user_registration_steps.rb
@@ -1,14 +1,13 @@
 Given("I am on user registration page") do
     visit new_user_path
-    
 end
   
 Given("I fill in {string} with {string}") do |field, value|
     fill_in field, with: value
 end
   
-Given("I click {string}") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+Given("I click {string}") do |element|
+    click_on element
 end
   
 Then("I am on {string} page") do |string|
