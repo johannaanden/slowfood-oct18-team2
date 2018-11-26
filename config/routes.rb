8eaf9ea27a8c3ba3921d130fa64d8da9c4b628a7
Aditya Naik <naik.aditya@gmail.com>
2018-11-26 20:42:38 +0100

    fixed user model issue by db rollback, destroy model user, added devise model user to use devise gem for auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'user/index'
   root controller: :users, action: :index
   resources :user
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :user do
-    email "MyString"
-    password "MyString"
+    
   end
 end
