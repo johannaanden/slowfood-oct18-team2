6369933627051a3a6903f2e7e4cfcfd2030f9627
Aditya Naik <naik.aditya@gmail.com>
2018-11-27 15:04:12 +0100

    Modified step definitions and feature file, tests going green

--- features/Step_definitions/user_registration_steps.rb
+++ features/Step_definitions/user_registration_steps.rb
@@ -2,16 +2,16 @@ Given("I am on user registration page") do
     visit new_user_registration_path
 end
   
-Given("I fill in {string} with {string}") do |string, string2|
-    pending # Write code here that turns the phrase above into concrete actions
+Given("I fill in {string} with {string}") do |element, value|
+    fill_in element, with: value
 end
   
-Given("I click {string}") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+Given("I click {string}") do |element|
+    click_on element
 end
   
-Then("I am on {string} page") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+Then("I am on landing page") do
+    visit root_path
 end
   
 Then("I see {string} alert") do |string|
--- features/user_registration.feature
+++ features/user_registration.feature
@@ -8,7 +8,7 @@ Scenario: User registration [Happy path]
     Given I am on user registration page
     And I fill in "Email" with "johanna@gmail.com"
     And I fill in "Password" with "password123"
-    And I fill in "Conformation" with "password123"
-    And I click "Create account"
-    Then I am on "landing" page
+    And I fill in "Password confirmation" with "password123"
+    And I click "Sign up"
+    Then I am on landing page
     And I see "User succesfully created" alert
