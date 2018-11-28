d2af4919e110eb2cabc3c759f5b3c8ef77f6eb60
johannaanden <johannaanden@hotmail.com>
2018-11-28 12:09:46 +0100

    Changing routes, solves route problems

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'home/index'
+  #get 'home/index'
   devise_for :users
-  root to: "home#index"
+  root to: "products#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     resources :products, only: [:index]
