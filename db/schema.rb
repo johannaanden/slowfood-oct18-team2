044655b63f109fe06d9f521bb31d12dd4fb03258
johannaanden <johannaanden@hotmail.com>
2018-11-28 11:53:14 +0100

    Fixes syntax error in schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2018_11_27_120546) do
-ActiveRecord::Schema.define(version: 2018_11_26_193034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "price"
+    t.text "name"
+    t.text "description"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,12 +33,6 @@ ActiveRecord::Schema.define(version: 2018_11_26_193034) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  create_table "products", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "price"
-    t.text "name"
-    t.text "description"
   end
 
 end
