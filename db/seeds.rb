2da24d8df7af5c26cd2f0adac82f152b1a7cb5a0
Aditya Naik <naik.aditya@gmail.com>
2018-11-29 16:03:01 +0100

    Added associations between products and catogories, added data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(Category: "Starters")
+Category.create(Category: "Main Course")
+Category.create(Category: "Desserts")
