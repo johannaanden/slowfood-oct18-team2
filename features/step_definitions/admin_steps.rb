1078132c9fddbd0adbaefc220d4823d90272a22f
Aditya Naik <naik.aditya@gmail.com>
2018-11-30 17:22:37 +0100

    Removed unwanted step from definitions file

--- features/step_definitions/admin_steps.rb
+++ features/step_definitions/admin_steps.rb
@@ -1,14 +1,9 @@
-Given("the owner exists") do |table|
-    # table is a Cucumber::MultilineArgument::DataTable
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  When("owner has logged in") do
-    steps %{
-        Given I visit login page 
-        And I fill in "Email" with 'odin@norsegod.com'
-        And I fill in "Password" with 'oneeye111'
-        And I click 'Log in'
-    }
-  end
+When("owner has logged in") do
+  steps %{
+      Given I visit login page 
+      And I fill in "Email" with 'odin@norsegod.com'
+      And I fill in "Password" with 'oneeye111'
+      And I click 'Log in'
+  }
+end
   
