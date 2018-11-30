557e8726594d6310f0cd029b53d12c9d0ca8650f
johannaanden <johannaanden@hotmail.com>
2018-11-30 17:29:40 +0100

    Removes unnecessary penfing test

--- features/step_definitions/admin_steps.rb
+++ features/step_definitions/admin_steps.rb
@@ -1,9 +1,4 @@
-Given("the owner exists") do |table|
-    # table is a Cucumber::MultilineArgument::DataTable
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  When("owner has logged in") do
+When("owner has logged in") do
     steps %{
         Given I visit login page 
         And I fill in "Email" with 'odin@norsegod.com'
