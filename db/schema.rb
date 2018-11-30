8885f757ebe2347bed3bcf87355d202427f11c9b
hanna_nyman <hannanyman.sk@gmail.com>
2018-11-30 15:11:39 +0100

    Migrating database after pulling from upstream

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2018_11_29_145747) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string "category"
     t.datetime "created_at", null: false
