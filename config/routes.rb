764fb391a74bbdc7f4a9c74b068cf497d8d414d3
johannaanden <johannaanden@hotmail.com>
2018-12-01 14:36:45 +0100

    Uppdates routes, and adds edit view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :products, only: [:index]
+  resources :products, only: [:index, :edit, :update]
   root controller: :products, action: :index
 end
