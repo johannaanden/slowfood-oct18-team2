985708c5d17f53923959dddd699270b02d1ce377
johannaanden <johannaanden@hotmail.com>
2018-12-02 12:13:29 +0100

    Uppdates feature test, adds more dishes to seeds file, uppdates link to uniqe link

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,17 @@ Category.create(category: "Starter")
 Category.create(category: "Main")
 Category.create(category: "Dessert")
 
-Product.create!(name: Faker::Food.dish, description: Faker::Food.description, price: Faker::Number.between(100, 300), category_id: 1)
-Product.create!(name: Faker::Food.dish, description: Faker::Food.description, price: Faker::Number.between(100, 300), category_id: 2)
-Product.create!(name: Faker::Food.dish, description: Faker::Food.description, price: Faker::Number.between(100, 300), category_id: 3)
+3.times do
+    Product.create!(name: Faker::Food.dish, description: Faker::Food.description, price: Faker::Number.between(100, 300), category_id: 1)
+end
+
+3.times do
+    Product.create!(name: Faker::Food.dish, description: Faker::Food.description, price: Faker::Number.between(100, 300), category_id: 2)
+end
+
+3.times do
+    Product.create!(name: Faker::Food.dish, description: Faker::Food.description, price: Faker::Number.between(100, 300), category_id: 3)
+end
 
 User.create(email: 'don@wh.se',password:'dondon111',owner: true)
 User.create(email: 'don@trump.com',password:'maga2020',owner: false)
